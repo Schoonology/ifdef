cc27772bcadfd42f51d3f485bce9c7de3f08dfc4
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-11-02 16:02:23 -0800

    Browser: Avoid module-level return statement.
    The behaviour around module-level return statements (that is, return statements
    outside the scope of a defined function) is ill-defined and not well supported
    across all CommonJS-compatible contexts (e.g. Webpack). This change avoids
    that return statement in tests for maximum support.

--- test/fixture.js
+++ test/fixture.js
@@ -2,13 +2,13 @@ var assert = require('assert')
 var ifdef = require('../')
 
 if (ifdef('ifdef_fixture')) {
-  return module.exports = ifdef('ifdef_fixture')
+  module.exports = ifdef('ifdef_fixture')
+} else {
+  assert(!global.required, 'Multiple require!')
+
+  global.required = true
+
+  module.exports = ifdef('ifdef_fixture', {
+    test: true
+  })
 }
-
-assert(!global.required, 'Multiple require!')
-
-global.required = true
-
-module.exports = ifdef('ifdef_fixture', {
-  test: true
-})
